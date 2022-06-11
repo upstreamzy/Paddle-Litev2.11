a361299d9cacbe7136ea158a07fea15c6f0dc90f
upstreamzy <upstreamzy@163.com>
2022-06-11 20:48:56 +0800

    xxx log

--- lite/model_parser/model_parser.cc
+++ lite/model_parser/model_parser.cc
@@ -248,6 +248,7 @@ void LoadModelPb(const std::string &model_dir,
   // Transform to cpp::ProgramDesc
   TransformProgramDescAnyToCpp(pb_prog, cpp_prog);
 #ifdef WITH_CONVERT_TO_SSA
+  printf("general::ssa::ConvertToSSA(cpp_prog) will be running \n");
   general::ssa::ConvertToSSA(cpp_prog);
 #endif
 
